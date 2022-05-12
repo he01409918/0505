76069f8bd7f98642262603b8e198fbcbd5ca1702
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-12 14:47:37 +0800

    Update GameCore.cs

--- GameCore.cs
+++ GameCore.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +7,24 @@ public class GameCore : MonoBehaviour
     public static GameCore Instance;
     [Header("玩家位置")]
     public Transform player;
-    /*
+    
     [Header("怪物殭屍Prefab")]
     public GameObject[] monsters;
     [Header("生成怪物的座標")]
-    public Transform[] zombiePoint;
+    public Transform[] monstersPoint;
     [Header("生成怪物間隔")]
-    public float nextZombieTime = 5;*/
+    public float nextZombieTime = 5;
 
     private void Start()
     {
         Instance = this;
-        InitZombie();
+        InitMonster();
     }
-    public void InitZombie()
+    public void InitMonster()
     {
-        /*int randomMonster = Random.Range(0, monsters.Length);
-        int randomPoint = Random.Range(0, zombiePoint.Length);
-        GameObject newZombie = Instantiate(monsters[randomMonster], zombiePoint[randomPoint].position, zombiePoint[randomPoint].rotation);
-        Invoke(nameof(InitZombie), nextZombieTime);*/
+        int randomMonster = Random.Range(0, monsters.Length);
+        int randomPoint = Random.Range(0, monstersPoint.Length);
+        GameObject newZombie = Instantiate(monsters[randomMonster], monstersPoint[randomPoint].position, monstersPoint[randomPoint].rotation);
+        Invoke(nameof(InitMonster), nextZombieTime);
     }
 }
