a3cb3eaa28a4bfa3a8fa646235969a5b71512a1f
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-12 14:06:03 +0800

    Add files via upload

---
+++ Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        nav.SetDestination(GameCore.Instance.player.position);
+    }
+}
