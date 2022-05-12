15fe747f8973390ce47692acb3015b1183b7d72c
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-12 15:02:08 +0800

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+    private Animator anim;
     private NavMeshAgent nav;
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+        Invoke(nameof(OnEnableAnimator), Random.Range(0f, 0.5f));
     }
 
     void Update()
@@ -18,5 +22,9 @@ public class Monster : MonoBehaviour
     private void Movement()
     {
         nav.SetDestination(GameCore.Instance.player.position);
+    }
+    private void OnEnableAnimator()
+    {
+        anim.enabled = true;
     }
 }
