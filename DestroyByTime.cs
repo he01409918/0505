d22e4eb424076047e7a6a008ce082444ac16a051
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-19 14:46:42 +0800

    Add files via upload

---
+++ DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    [Header("銷毀時間")]
+    public float timer;
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+}
