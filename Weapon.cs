023029dd8b6398f655f8a450315932e116755d38
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-19 14:26:18 +0800

    Add files via upload

---
+++ Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float damage = 100;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Monster"))
+        {
+            Monster monster = other.GetComponent<Monster>();
+            monster.OnGetHit(damage);
+        }
+    }
+}
