7e41f4857e1b0833f9d284172c74fefc4753c7ff
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-19 14:00:49 +0800

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -7,11 +7,18 @@ public class Monster : MonoBehaviour
 {
     private Animator anim;
     private NavMeshAgent nav;
+
+    public float hp = 100f;
+
+    private Rigidbody rb;
+
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         anim.enabled = false;
+        hp = 100;
         Invoke(nameof(OnEnableAnimator), Random.Range(0f, 0.5f));
     }
 
@@ -21,10 +28,37 @@ public class Monster : MonoBehaviour
     }
     private void Movement()
     {
-        nav.SetDestination(GameCore.Instance.player.position);
+        if (nav != null &&hp>0)
+        {
+            nav.SetDestination(GameCore.Instance.player.position);
+        }
     }
     private void OnEnableAnimator()
     {
         anim.enabled = true;
     }
+    public void OnGetHit(float value)
+    {
+        if (hp > 0)
+        {
+            hp -= value;
+            if (hp <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    private void Die()
+    {
+        anim.enabled = false;
+        rb.isKinematic = false;
+        Vector3 randomV3 = new Vector3(Random.Range(-5f, 5f), Random.Range(3f, 5f), Random.Range(-5f, 5f));
+        rb.AddForce(randomV3 , ForceMode.Impulse);
+        rb.AddTorque(randomV3  , ForceMode.Impulse);
+        nav.speed = 0;
+        nav.enabled = false;
+        Destroy(gameObject, 5);
+    }
+
 }
