e19a5695448c3f299ba390a762e79a93bb58fcbe
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-05-26 13:26:21 +0800

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -9,6 +9,7 @@ public class Monster : MonoBehaviour
     private Animator anim;
     private NavMeshAgent nav;
     private Rigidbody rb;
+    private Renderer rend;
 
     public float hp = 100f;
 
@@ -19,8 +20,8 @@ public class Monster : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        rend = GetComponentInChildren<Renderer>();
         anim.enabled = false;
-        hp = 100;
         Invoke(nameof(OnEnableAnimator), Random.Range(0f, 0.5f));
     }
 
@@ -47,6 +48,7 @@ public class Monster : MonoBehaviour
     {
         if (hp > 0)
         {
+            StartCoroutine(DoSkinFlash());
             hp -= value;
             if (hp <= 0)
             {
@@ -57,15 +59,25 @@ public class Monster : MonoBehaviour
 
     private void Die()
     {
+        GameCore.Instance.OnUpdateKillText(1);
         Instantiate(getHitEffect, transform.position + Vector3.up *1, transform.rotation);
-        anim.enabled = false;
+        
+        /*anim.enabled = false;
         rb.isKinematic = false;
         Vector3 randomV3 = new Vector3(Random.Range(-5f, 5f), Random.Range(3f, 5f), Random.Range(-5f, 5f));
         rb.AddForce(randomV3 , ForceMode.Impulse);
         rb.AddTorque(randomV3  , ForceMode.Impulse);
         nav.speed = 0;
         nav.enabled = false;
-        Destroy(gameObject, 5);
+        */
+        Destroy(gameObject);
+    }
+
+    IEnumerator DoSkinFlash()
+    {
+        rend.material.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        rend.material.color = Color.white; 
     }
 
 }
